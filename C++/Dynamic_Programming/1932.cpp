a4c31184b3fb6e491ecb752d1240f592d5608b6c
Kim <48668211+SkydevilK@users.noreply.github.com>
2020-01-07 16:19:39 +0900

    Create 1932.cpp

---
+++ C++/Dynamic_Programming/1932.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int max(int a, int b) {
+	return a > b ? a : b;
+}
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int N, result = 0;
+	cin >> N;
+	vector<vector<int> > arr(N + 1, vector<int>(N + 1, 0));
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= i; j++) {
+			cin >> arr[i][j];
+			arr[i][j] += max(arr[i - 1][j - 1], arr[i - 1][j]);
+			if (i == N) {
+				result = max(result, arr[i][j]);
+			}
+		}
+	}
+	cout << result;
+	return 0;
+}
